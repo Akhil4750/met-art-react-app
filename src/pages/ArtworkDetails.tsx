37b79d338d989f5fbf7068acd05bc16057040282
Akhil <atiru@my.bridgeport.edu>
2024-06-25 17:17:19 -0400

    Add functionality to save to favorites only once and change button color to green

--- src/pages/ArtworkDetails.tsx
+++ src/pages/ArtworkDetails.tsx
@@ -8,6 +8,8 @@ import './ArtworkDetails.css';
 const ArtworkDetails: React.FC = () => {
     const { objectId } = useParams<{ objectId: string }>();// Retrieve objectId from URL parameters
     const [artwork, setArtwork] = useState<Artwork | null>(null); // State to store artwork details
+    const [isFavorite, setIsFavorite] = useState(false); // State to check if artwork is already in favorites
+
 
 
     useEffect(() => {
@@ -16,6 +18,10 @@ const ArtworkDetails: React.FC = () => {
             if (objectId) {
                 const data = await fetchArtworkDetails(parseInt(objectId));
                 setArtwork(data);
+                // Check if artwork is already in favorites
+                const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+                const favoriteExists = favorites.some((fav: Artwork) => fav.objectID === data.objectID);
+                setIsFavorite(favoriteExists); // Set isFavorite state
             }
         };
         getArtworkDetails();
@@ -28,6 +34,7 @@ const ArtworkDetails: React.FC = () => {
         const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
         favorites.push(artwork);
         localStorage.setItem('favorites', JSON.stringify(favorites));
+        setIsFavorite(true); // Update isFavorite state
     };
 
     return (
@@ -43,7 +50,14 @@ const ArtworkDetails: React.FC = () => {
                     <span className="artist-date">{artwork.objectDate}</span>
                 </p>
                 <p className="artwork-description">{artwork.description}</p>
-                <button onClick={saveToFavorites} className="save-button">Save to Favorites</button>
+
+                <button
+                    onClick={saveToFavorites}
+                    className={`save-button ${isFavorite ? 'favorite' : ''}`} // Applied favorite class conditionally
+                    disabled={isFavorite} // Disable button if already in favorites
+                >
+                    {isFavorite ? "Added to Favorites" : "Save to Favorites"}
+                </button>
             </div>
         </div>
     );
